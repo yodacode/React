ce2369f4f9890ff34274cdb277f901b3aa4ad6cc
bdevaublanc <bdevaublanc@molotov.tv>
2015-07-15 22:12:06 +0200

    Hello workd done

---
+++ build/.module-cache/21857c10c4d910134e023a1c96e0b31a2166c187.js
@@ -0,0 +1,4 @@
+React.render(
+  React.createElement("h1", null, "Hello, world Benjamin!"),
+  document.getElementById('container')
+);
---
+++ src/helloworld.js
@@ -0,0 +1,4 @@
+React.render(
+  <h1>Hello, world Benjamin!</h1>,
+  document.getElementById('container')
+);
