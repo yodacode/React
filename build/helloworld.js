b8675f01224cbc5d377609f134a1ae08f98c99ab
bdevaublanc <bdevaublanc@molotov.tv>
2015-07-15 23:17:05 +0200

    start tutorial

--- build/helloworld.js
+++
@@ -1,4 +0,0 @@
-React.render(
-  React.createElement("h1", null, "Hello, world Benjamin!"),
-  document.getElementById('container')
-);
--- src/helloworld.js
+++
@@ -1,4 +0,0 @@
-React.render(
-  <h1>Hello, world Benjamin!</h1>,
-  document.getElementById('container')
-);
